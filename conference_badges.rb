f8cb8040c79e0065392a5313ec352e3a158b19ec
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 15:26:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,9 @@ end
 def assign_rooms(array)
     
 end 
+
+hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
