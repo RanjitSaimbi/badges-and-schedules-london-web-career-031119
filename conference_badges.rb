c1b11c69e5f0b96245a462e03bcd6301e6843430
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-19 15:36:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,3 +17,10 @@ def assign_rooms(array)
 }
 return newArray
 end 
+
+def printer(attendees) 
+  array1 = batch_badge_creator(attendees)
+  array2 = assign_rooms(attendees)
+  puts array1
+  puts array2
+end 
